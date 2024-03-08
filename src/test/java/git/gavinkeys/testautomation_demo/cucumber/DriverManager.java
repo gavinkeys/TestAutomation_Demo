c915f88430756e10c8164e5c79412f0efb7de5c3
Gavin <gdkeys6@gmail.com>
2024-03-08 16:24:31 +0000

    Improved DriverManager and added comments + dependency to POM

--- src/test/java/git/gavinkeys/testautomation_demo/cucumber/DriverManager.java
+++ src/test/java/git/gavinkeys/testautomation_demo/cucumber/DriverManager.java
@@ -7,22 +7,42 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class DriverManager {
     private WebDriver driver;
 
-    public DriverManager(String browser) {
-        initializeDriver(browser);
-    }
-
+    /**
+     * Initializes the WebDriver based on the specified browser.
+     * @param browser The browser to use ("chrome" or "firefox").
+     * @throws IllegalArgumentException if an invalid browser is specified.
+     */
     private void initializeDriver(String browser) {
+        String driversPath = System.getProperty("user.dir") + "/src/test/java/driver/";
+
         if (browser.equalsIgnoreCase("chrome")) {
-            System.setProperty("webdriver.chrome.driver", "/Users/gavinkeys/Documents/MoJ/TestAutomation_Demo/src/test/java/driver/chromedriver");
+            // Set the system property for ChromeDriver
+            System.setProperty("webdriver.chrome.driver", driversPath + "chromedriver");
+            // Initialize ChromeDriver
             driver = new ChromeDriver();
         } else if (browser.equalsIgnoreCase("firefox")) {
-            System.setProperty("webdriver.gecko.driver", "/Users/gavinkeys/Documents/MoJ/TestAutomation_Demo/src/test/java/driver/geckodriver");
+            // Set the system property for GeckoDriver
+            System.setProperty("webdriver.gecko.driver", driversPath + "geckodriver");
+            // Initialize FirefoxDriver
             driver = new FirefoxDriver();
         } else {
+            // Throw an exception for an invalid browser
             throw new IllegalArgumentException("Invalid browser specified");
         }
     }
 
+    /**
+     * Constructor that initializes the WebDriver with the specified browser.
+     * @param browser The browser to use (e.g., "chrome" or "firefox").
+     */
+    public DriverManager(String browser) {
+        initializeDriver(browser);
+    }
+
+    /**
+     * Retrieves the initialized WebDriver instance.
+     * @return The WebDriver instance.
+     */
     public WebDriver getDriver() {
         return driver;
     }
