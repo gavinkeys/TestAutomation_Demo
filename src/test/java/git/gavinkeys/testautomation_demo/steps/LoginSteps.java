ad13129a23e3f3212359d9a3d375e40b0b643d1c
Gavin <gdkeys6@gmail.com>
2024-03-08 16:36:47 +0000

    Updated step classes to use TestHooks + added feature scenarios

--- src/test/java/git/gavinkeys/testautomation_demo/steps/LoginSteps.java
+++ src/test/java/git/gavinkeys/testautomation_demo/steps/LoginSteps.java
@@ -1,63 +1,94 @@
 package git.gavinkeys.testautomation_demo.steps;
 
-import git.gavinkeys.testautomation_demo.cucumber.DriverManager;
+import git.gavinkeys.testautomation_demo.cucumber.TestHooks;
 import git.gavinkeys.testautomation_demo.pages.BasePage;
 import git.gavinkeys.testautomation_demo.pages.LoginPage;
-import io.cucumber.java.After;
-import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class LoginSteps {
 
     private LoginPage loginPage;
-    private BasePage basePage;
+    private final TestHooks testHooks;
+    private final BasePage basePage;
 
-    @Before
-    public void setUp() {
-        // Initialize WebDriver before each scenario
-        String browser = System.getProperty("browser", "chrome");
-
-        // Initialize WebDriver using the DriverManager class
-        DriverManager driverManager = new DriverManager(browser);
-        basePage = new BasePage(driverManager);
-        loginPage = new LoginPage(driverManager);
+    // Constructor to initialize TestHooks and BasePage
+    public LoginSteps(TestHooks testHooks) {
+        this.testHooks = testHooks;
+        this.basePage = testHooks.getBasePage();
+        this.loginPage = testHooks.getLoginPage();
     }
 
-    @After
-    public void tearDown() {
-        // Close WebDriver after each scenario
-        if (basePage != null) {
-            WebDriver driver = basePage.getDriver();
-            if (driver != null) {
-                driver.quit();
-            }
-        }
-    }
+    // Step Definitions
 
+    /**
+     * Given step: Navigates to the specified URL.
+     *
+     * @param url The URL to navigate to.
+     */
     @Given("I navigate to {string}")
     public void i_navigate_to(String url) {
-        loginPage.navigateToURL(url);
+        basePage.navigateToURL(url);
     }
 
+    /**
+     * Given step: Enters the provided username into the username input field.
+     *
+     * @param username The username to enter.
+     */
     @Given("I enter the username {string}")
     public void i_enter_the_username(String username) {
         loginPage.enterUsername(username);
     }
 
+    /**
+     * Given step: Enters the provided password into the password input field.
+     *
+     * @param password The password to enter.
+     */
     @Given("I enter the password {string}")
     public void i_enter_the_password(String password) {
         loginPage.enterPassword(password);
     }
 
+    /**
+     * Given step: Performs a full login by entering the username, password, and clicking the login button.
+     *
+     * @param username The username to enter.
+     * @param password The password to enter.
+     */
     @Given("I login to saucedemo")
     public void i_login_to_saucedemo(String username, String password) {
         loginPage.fullLogin(username, password);
     }
 
+    /**
+     * Verifies that the user is on the login page by checking the visibility of the login page elements.
+     */
+    @And("I am on the login page")
+    public void i_am_on_the_login_page() {
+        loginPage.isLoginPageVisible();
+    }
+
+    /**
+     * Then step: Clicks the login button.
+     */
     @Then("I click the login button")
     public void i_click_the_login_button() {
         loginPage.clickLogin();
+    }
+
+    /**
+     * Then step: Verifies if the specified error message is displayed.
+     *
+     * @param errorMessage The expected error message.
+     */
+    @Then("I see the {string} error message")
+    public void i_see_the_error_message(String errorMessage) {
+        WebElement errorMessageElement = loginPage.getErrorMessageElement();
+        
+        loginPage.errorMessageDisplayed(errorMessageElement);
     }
 }
--- src/test/resources/features/login.feature
+++ src/test/resources/features/login.feature
@@ -3,6 +3,7 @@ Feature: Login Feature
 
   Background:
     Given I navigate to "https://www.saucedemo.com/"
+    And I am on the login page
 
   @HappyPath
   Scenario Outline: Login to SauceDemo.com
@@ -10,11 +11,59 @@ Feature: Login Feature
     And I enter the password <password>
     Then I click the login button
 
+    Given I have successfully logged in
+    Then I logout
+
     Examples:
-      | username                  | password       |
-      | "standard_user"           | "secret_sauce" |
-      | "locked_out_user"         | "secret_sauce" |
-      | "problem_user"            | "secret_sauce" |
-      | "performance_glitch_user" | "secret_sauce" |
-      | "error_user"              | "secret_sauce" |
-      | "visual_user"             | "secret_sauce" |
+      | username        | password       |
+      | "standard_user" | "secret_sauce" |
+      | "error_user"    | "secret_sauce" |
+
+  @LockedOutUser
+  Scenario Outline: Login to SauceDemo.com with locked-out user
+    Given I enter the username <username>
+    And I enter the password <password>
+    And I click the login button
+    Then I see the "Epic sadface: Sorry, this user has been locked out." error message
+
+    Examples:
+      | username          | password        |
+      | "locked_out_user" | "secret_sauce"  |
+      | "standard_user"   | "locked_out_pwd"|
+
+  @EmptyCredentials
+  Scenario: Login to SauceDemo.com with empty credentials
+    Given I click the login button
+    Then I see the "Epic sadface: Username is required" error message
+
+  @InvalidUsername
+  Scenario Outline: Login to SauceDemo.com with invalid username
+    Given I enter the username <username>
+    And I enter the password "secret_sauce"
+    And I click the login button
+    Then I see the "Epic sadface: Username and password do not match any user in this service" error message
+
+    Examples:
+      | username                   |
+      | "invalid_characters_$%"    |
+      | "invalid_email_format.com" |
+      | " "                        |
+
+  @InvalidPassword
+  Scenario Outline: Login to SauceDemo.com with invalid password
+    Given I enter the username "standard_user"
+    And I enter the password <password>
+    And I click the login button
+    Then I see the "Epic sadface: Username and password do not match any user in this service" error message
+
+    Examples:
+      | password                   |
+      | "short"                    |
+      | "long_password"            |
+      | " "                        |
+
+  @InvalidSession
+  Scenario: Invalid session attempt
+    When I access a secure page without logging in
+    Then I am on the login page
+    And I see the "Epic sadface: You can only access '/inventory-item.html' when you are logged in." error message
